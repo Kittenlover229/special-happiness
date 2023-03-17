db6342b3e0f3641e20184d3273a7a8be3ce3cbe3
Kittenlover229 <artur.roos@mail.ru>
2023-03-17 20:22:44 +0200

    Add camera controls

--- Assets/Scripts/Controls/CameraController.cs
+++ Assets/Scripts/Controls/CameraController.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Camera Camera; 
+    public Camera Camera;
+    public Transform Pivot;
+    public float CameraMovementSpeed;
 
     void Start()
     {
@@ -14,6 +14,17 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        var verticalAxis = vertical * Vector3.ProjectOnPlane(Camera.transform.forward, Vector3.up).normalized;
+        var horizontalAxis = horizontal * Vector3.ProjectOnPlane(Camera.transform.right, Vector3.up).normalized;
+
+        var movement = (verticalAxis + horizontalAxis) * Time.deltaTime * CameraMovementSpeed;
+        Pivot.transform.Translate(movement, Space.World);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(Pivot.position, 0.1f);
     }
 }
