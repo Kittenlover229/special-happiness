71db87c6497c650a5a91246b746a6d90f6c9fe80
Kittenlover229 <artur.roos@mail.ru>
2023-03-17 23:29:56 +0200

    Make cards' positions not pixel-dependent

--- Assets/Scripts/Controllers/CardsController.cs
+++ Assets/Scripts/Controllers/CardsController.cs
@@ -2,41 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+interface ICardControllerState
+{
+    abstract ICardControllerState Update();
+}
+
+[System.Serializable]
+public class CardAnimationParameters
+{
+    public Vector2Int CardOffsetFractionOfTheScreen;
+    public Vector2 CardSpread;
+    public float maxCardRotation;
+    public AnimationCurve cardArcCurve;
+    public float cardArcLift;
+    public float cardMovementSmoothness;
+}
+
+class IdleState : ICardControllerState
+{
+    CardsController controller;
+
+    public IdleState(CardsController controller) {
+        this.controller = controller;
+    }
+
+    public ICardControllerState Update()
+    {
+        var parameters = controller.Parameters;
+        var cardCount = controller.Cards.Count;
+        var cards = controller.Cards;
+
+        // TODO: center this bullshit on the screen
+        float iters = cardCount - 1;
+        for (int i = 0; i < cardCount; i++)
+        {
+            Rect canvasRect = controller.Canvas.pixelRect;
+            Vector3 newPosition = new Vector2(canvasRect.center.x, 0) 
+                + Vector2.up * canvasRect.height / parameters.CardOffsetFractionOfTheScreen.y * parameters.CardOffsetFractionOfTheScreen.x
+                + i * (parameters.CardSpread / iters)
+                - parameters.CardSpread / 2
+                - Vector2.up * (parameters.cardArcCurve.Evaluate(Mathf.Abs(1 - i / iters * 2)) - 1) * parameters.cardArcLift;
+
+            cards[i].transform.position =
+                Vector3.Lerp(
+                    cards[i].transform.position,
+                    newPosition,
+                    Time.deltaTime * parameters.cardMovementSmoothness);
+
+            Quaternion newRotation =
+                Quaternion.Euler(
+                    0, 0,
+                    ((float)i - cardCount / 2) * parameters.maxCardRotation);
+
+            cards[i].transform.rotation = newRotation;
+        }
+
+        return this;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.matrix = controller.Canvas.transform.localToWorldMatrix;
+    }
+}
+
+class CardSelectedState : ICardControllerState
+{
+    public ICardControllerState Update()
+    {
+        return this;
+    }
+}
+
+[ExecuteInEditMode]
 public class CardsController : MonoBehaviour
 {
-    [SerializeField]
-    Canvas canvas;
-    [SerializeField]
-    Vector2 cardOffset;
-    [SerializeField]
-    Vector2 cardSpread;
-    [SerializeField]
-    float maxCardRotation;
-    [SerializeField]
-    List<Card> cards = new List<Card>();
-    [SerializeField]
-    AnimationCurve cardArcCurve;
-    [SerializeField]
-    float cardArcLift;
-    [SerializeField]
-    float cardMovementSmoothness;
+    public Canvas Canvas;
+    public CardAnimationParameters Parameters;
+    public List<Card> Cards = new List<Card>();
+
+    ICardControllerState State = null;
+
+    void Awake()
+    {
+        State = new IdleState(this);
+    }
 
     void Update()
     {
-        // TODO: center this bullshit on the screen
-        float iters = cards.Count - 1;
-        for (int i = 0; i < cards.Count; i++)
-        {
-            float d = i / iters;
-            Vector3 newPosition = cardOffset - i * (cardSpread / iters) + cardSpread / 2 + Vector2.up * cardArcCurve.Evaluate(Mathf.Abs(1 - d * 2)) * cardArcLift;
-            cards[i].transform.position = Vector3.Lerp(cards[i].transform.position, newPosition, Time.deltaTime * cardMovementSmoothness);
-            Quaternion newRotation = Quaternion.Euler(0, 0, ((float)i - cards.Count / 2) * maxCardRotation);
-            cards[i].transform.rotation = newRotation;
-        }
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.matrix = canvas.transform.localToWorldMatrix;
+        if (State == null)
+            State = new IdleState(this);
+        State = State.Update();
     }
 }
