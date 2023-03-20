666aa5420e35037a5d2603f8538d481c4951cfdc
Kittenlover229 <artur.roos@mail.ru>
2023-03-20 23:21:59 +0200

    Properly handle any amount of cards

--- Assets/Scripts/Controllers/CardsController.cs
+++ Assets/Scripts/Controllers/CardsController.cs
@@ -12,84 +12,93 @@ public interface ICardControllerState
 [System.Serializable]
 public class CardAnimationParameters
 {
+    // So this is UI, so position of the cards on the screen is defined rather 
+    // weirdly. The screen is separated into .y equally sized horizontal 
+    // stripes and .x defines at the bottom of which stripe the cards are
     public Vector2Int CardOffsetFractionOfTheScreen;
-    public Vector2 CardSpread;
-    public float maxCardRotation;
-    public AnimationCurve cardArcCurve;
-    public float cardArcLift;
-    public float cardMovementSmoothness;
+    public float CardSpread;
+    public float MaxCardRotation;
+    public AnimationCurve CardArcCurve;
+    public float CardArcLift;
+    public float CardMovementSmoothness;
     public float CardRotationSpeed;
     public float HoverUp;
+    public float CardPrefferedDistance;
 }
 
 class IdleState : ICardControllerState
 {
-    CardsController controller;
-    List<Card> hoveredCards = new();
+    CardsController Controller;
+    Card HoveredCard = null;
 
     public IdleState(CardsController controller)
     {
-        this.controller = controller;
+        this.Controller = controller;
     }
 
     public ICardControllerState Update()
     {
-        var parameters = controller.Parameters;
-        var cardCount = controller.Cards.Count;
-        var cards = controller.Cards;
+        var parameters = Controller.Parameters;
+        var cardCount = Controller.Cards.Count;
+        var cards = Controller.Cards;
+
+        int inactiveCards = cards.FindAll((card) => !card.gameObject.activeInHierarchy).Count;
 
         // TODO: center this bullshit on the screen
-        float iters = cardCount - 1;
+        int inactiveCardsEncountered = 0;
+        int iters = cardCount - 1 - inactiveCards;
         for (int i = 0; i < cardCount; i++)
         {
-            bool isCardHovered = hoveredCards.Contains(cards[i]);
+            if (!cards[i].gameObject.activeInHierarchy) {
+                inactiveCardsEncountered += 1;
+                continue;
+            }
 
-            Rect canvasRect = controller.Canvas.pixelRect;
+            Card card = cards[i];
+            bool isCardHovered = HoveredCard == card;
+            i -= inactiveCardsEncountered;
+
+            float horizontalSpread = Mathf.Min(parameters.CardSpread / iters, parameters.CardPrefferedDistance);
+            float maxHorizontalSpread = horizontalSpread * iters;
+
+            Rect canvasRect = Controller.Canvas.pixelRect;
             Vector3 newPosition = new Vector2(canvasRect.center.x, 0)
                 + Vector2.up * canvasRect.height / parameters.CardOffsetFractionOfTheScreen.y * parameters.CardOffsetFractionOfTheScreen.x
-                + i * (parameters.CardSpread / iters)
-                - parameters.CardSpread / 2
-                - Vector2.up * (parameters.cardArcCurve.Evaluate(Mathf.Abs(1 - i / iters * 2)) - 1) * parameters.cardArcLift
+                + Vector2.right * (i * horizontalSpread - maxHorizontalSpread / 2)
+                - Vector2.up * (parameters.CardArcCurve.Evaluate(Mathf.Abs(1 - i / (float)(iters != 0 ? iters : 1) * 2)) - 1) * parameters.CardArcLift
                 + (isCardHovered ? (Vector2.up * parameters.HoverUp) : Vector3.zero);
 
 
-            cards[i].transform.position =
+            card.transform.position =
                 Vector3.Lerp(
-                    cards[i].transform.position,
+                    card.transform.position,
                     newPosition,
-                    Time.deltaTime * parameters.cardMovementSmoothness);
+                    Time.deltaTime * parameters.CardMovementSmoothness);
 
-            var rotation = hoveredCards.Contains(cards[i]) ? Quaternion.identity :
+            var rotation = isCardHovered ? Quaternion.identity :
                 Quaternion.Euler(
                     0, 0,
-                    ((float)i - cardCount / 2) * parameters.maxCardRotation);
+                    (i - (float)iters / 2) * parameters.MaxCardRotation);
 
-            cards[i].transform.rotation = Quaternion.Lerp(cards[i].transform.rotation, rotation, Time.deltaTime * parameters.CardRotationSpeed);
+            card.transform.rotation = Quaternion.Lerp(card.transform.rotation, rotation, Time.deltaTime * parameters.CardRotationSpeed);
+            i += inactiveCardsEncountered;
         }
 
         return this;
     }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.matrix = controller.Canvas.transform.localToWorldMatrix;
-        foreach (Card card in hoveredCards)
-        {
-            Gizmos.DrawSphere(card.rect.anchorMin, 0.5f);
-        }
-    }
-
     public void NotifyEnterCard(Card card)
     {
-        hoveredCards.Add(card);
+        HoveredCard = card;
         card.transform.SetAsLastSibling();
     }
 
     public void NotifyExitCard(Card card)
     {
-        hoveredCards.Remove(card);
-        for (int i = 0; i < this.controller.Cards.Count; i++) {
-            this.controller.Cards[i].transform.SetSiblingIndex(i);
+        HoveredCard = null;
+        for (int i = 0; i < this.Controller.Cards.Count; i++)
+        {
+            this.Controller.Cards[i].transform.SetSiblingIndex(i);
         }
     }
 }
